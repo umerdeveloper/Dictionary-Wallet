2285d8fa24a6989f8bf7bfd0563fd9ffc5199fb5
Umer Khan <umar_s66@outlook.com>
2020-04-02 10:53:49 +0500

    Initial Commit

---
+++ Dictionary Wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dictionary Wallet
+//
+//  Created by Umer Khan on 02/04/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
